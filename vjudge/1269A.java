7a664f410e23f986cc5617555fec0e32bde77960
Ikjot Singh Dhody <ikjotsd@gmail.com>
2021-05-17 23:34:52 +0530

    Ishaan init

--- vjudge/1269A.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.*;
-public class TuxCoder {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        long n=sc.nextLong();
-        System.out.println((9*n)+" "+(8*n));
-    }
-    }
--- vjudge/1293B.java
+++
@@ -1,7 +0,0 @@
-/*@author Ikjot*/
-import java.util.*;
-public class c_20_3vj{
-    public static void main(String[] args){
-        double t=(new Scanner(System.in)).nextInt(),r=0,i=1;
-        for(;i<=t;i++){r+=1/i;}
-System.out.println(r);}}
